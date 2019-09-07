635922b1ea167840341ab262f92de754c1d4cc2d
kokiebisu <kenichikona@gmail.com>
2019-09-07 15:22:48 -0700

    fix

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const path = require('path');
 
 app.use(cors());
 app.use(express.json());
+
+if (process.env.NODE_ENV === 'production') {
+  // Serve statuc files from the React frontend app
+  app.use(express.static(path.join(__dirname, 'client/build')));
+
+  // Anything that doesn't match the above, send back index.html
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'));
+  });
+}
 
 module.exports = app;
