ce7c61e9918812767b4164c13c9e0fdebe846a28
kokiebisu <kenichikona@gmail.com>
2019-09-07 22:15:30 -0700

    added headerboard

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,12 +1,74 @@
 import React from 'react';
 import './header.scss';
+import { Autocomplete, TextInput } from 'evergreen-ui';
 
 export default function Header() {
   return (
     <>
       <section className="header__container">
-        <div className="header">
-          <div className="header__title">Welcome</div>
+        <div className="headerboard__container">
+          <div className="headerboard">
+            <h3 className="headerboard__heading">
+              Learn about the course. <br />
+              Don't waste your time.
+            </h3>
+            <Autocomplete
+              title="Suggestion"
+              onChange={changedItem => console.log(changedItem)}
+              items={[
+                'Computer Systems Technology',
+                'Computer Information Technology'
+              ]}
+              itemSize={45}
+            >
+              {props => {
+                const { getInputProps, getRef, inputValue, openMenu } = props;
+                return (
+                  <TextInput
+                    placeholder="Program"
+                    value={inputValue}
+                    innerRef={getRef}
+                    height={50}
+                    width="100%"
+                    marginBottom={30}
+                    disabled
+                    {...getInputProps({
+                      onFocus: () => {
+                        openMenu();
+                      }
+                    })}
+                  />
+                );
+              }}
+            </Autocomplete>
+
+            <Autocomplete
+              title="Suggestion"
+              onChange={changedItem => console.log(changedItem)}
+              items={['Object Oriented Programming 2', 'Relational Database']}
+              itemSize={45}
+            >
+              {props => {
+                const { getInputProps, getRef, inputValue, openMenu } = props;
+                return (
+                  <TextInput
+                    placeholder="Course"
+                    value={inputValue}
+                    innerRef={getRef}
+                    height={50}
+                    marginBottom={50}
+                    width="100%"
+                    {...getInputProps({
+                      onFocus: () => {
+                        openMenu();
+                      }
+                    })}
+                  />
+                );
+              }}
+            </Autocomplete>
+            <button className="headerboard__button">Search</button>
+          </div>
         </div>
       </section>
     </>
