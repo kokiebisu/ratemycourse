2973f6c13eadcdd9faab9d075b3e5779f6306518
Ken <a01056715@gmail.com>
2020-05-26 15:37:38 -0700

    recreated course schema

--- src/models/Course.ts
+++ src/models/Course.ts
@@ -7,5 +7,36 @@ export const CourseSchema = new Schema({
     type: String,
     required: true,
   },
-  professor: {},
+  professor: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  happy: {
+    type: Number,
+    required: true,
+  },
+  sad: {
+    type: Number,
+    required: true,
+  },
+  angry: {
+    type: Number,
+    required: true,
+  },
+  everydayStudy: {
+    type: Number,
+    required: true,
+  },
+  midtermStudy: {
+    type: Number,
+    required: true,
+  },
+  finalStudy: {
+    type: Number,
+    required: true,
+  },
 });
