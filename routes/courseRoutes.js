b52efc63454dae16944ff62c0e9271af3a225b59
Ken <a01056715@gmail.com>
2020-05-25 21:00:47 -0700

    organized server side

--- routes/courseRoutes.js
+++ routes/courseRoutes.js
@@ -1,52 +1,51 @@
 const mongoose = require('mongoose');
+
+const Router = mongoose.Router();
 const Course = mongoose.model('courses');
 
-module.exports = app => {
-  app.get('/courses', async (req, res) => {
-    console.log('received');
-    try {
-      const courses = await Course.find();
-      res.status(200).json({
-        status: 'success',
-        courses
-      });
-    } catch (err) {
-      res.status(404).json({
-        status: 'error',
-        message: err
-      });
-    }
-  });
+Router.get('/', async (req, res) => {
+  try {
+    const courses = await Course.find();
+    res.status(200).json({
+      status: 'success',
+      courses,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'error',
+      message: err,
+    });
+  }
+});
 
-  app.post('/courses', async (req, res) => {
-    try {
-      const newCourse = await Course.create(req.body);
-      res.status(200).json({
-        status: 'success',
-        newCourse
-      });
-    } catch (err) {
-      res.status(404).json({
-        status: 'error',
-        message: err
-      });
-    }
-  });
+Router.post('/', async (req, res) => {
+  try {
+    const newCourse = await Course.create(req.body);
+    res.status(200).json({
+      status: 'success',
+      newCourse,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'error',
+      message: err,
+    });
+  }
+});
 
-  app.get('/courses/:id', async (req, res) => {
-    try {
-      id = req.params.id;
-      console.log();
-      const course = await Course.findById(req.params.id);
-      res.status(200).json({
-        status: 'success',
-        course
-      });
-    } catch (err) {
-      res.status(404).json({
-        status: 'error',
-        message: err
-      });
-    }
-  });
-};
+Router.get('/:id', async (req, res) => {
+  try {
+    id = req.params.id;
+    console.log();
+    const course = await Course.findById(req.params.id);
+    res.status(200).json({
+      status: 'success',
+      course,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'error',
+      message: err,
+    });
+  }
+});
