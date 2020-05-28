d57954d376475b7a1c1e85c332b096e59b71fc7a
Ken <a01056715@gmail.com>
2020-05-27 22:00:15 -0700

    added breakpoint utility

--- client/src/components/Landing/header.tsx
+++ client/src/components/Landing/header.tsx
@@ -1,5 +1,9 @@
 import * as React from 'react';
 
 export const Header = () => {
-  return <div className='bg--primary h-48'>what is it </div>;
+  return (
+    <div className='md--bg-secondary h-24'>
+      <div className='mx-4 bg--primary'>he</div>
+    </div>
+  );
 };
--- client/src/components/Landing/index.tsx
+++ client/src/components/Landing/index.tsx
@@ -3,10 +3,18 @@ import * as React from 'react';
 // Components
 import { Header } from './header';
 
+// Svg
+import { TopShape } from '../../assets/svgs/Shapes';
+
 export const Landing: React.FC<{}> = () => {
   return (
     <>
-      <Header />
+      <div className='w-screen h-auto'>
+        <TopShape color='#414381' />
+      </div>
+      <div style={{ position: 'absolute', top: 0, width: '100vw' }}>
+        <Header />
+      </div>
     </>
   );
 };
