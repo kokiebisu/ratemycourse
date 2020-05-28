769f7a4d5b76872a5de908f5e9e5c6e03c7dbc82
Ken <a01056715@gmail.com>
2020-05-28 01:18:34 -0700

    added borders to utility

--- client/src/components/Landing/header.tsx
+++ client/src/components/Landing/header.tsx
@@ -2,9 +2,16 @@ import * as React from 'react';
 
 export const Header = () => {
   return (
-    <div className='mx-auto h-16'>
-      <div className='flex'>
-        <div className='mx-4 bg--primary'>he</div>
+    <div className='mx-auto'>
+      <div className='mx-8'>
+        <div className='flex justify-between items-center h-16'>
+          <div className='bg--primary'>
+            <h3 className='font-medium font-2xl color-white'>ratemycourse</h3>
+          </div>
+          <div className='bg-lightblue'>
+            <button className='color-white'>button</button>
+          </div>
+        </div>
       </div>
     </div>
   );
